9331df25c46723d400a009a21516b1a940ce04fe
ctfrommn <chadclough@gmail.com>
2014-06-07 20:27:32 -0500

    Clean up vendor files

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,7 +2,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/velocity_mako.mk \
     $(LOCAL_DIR)/velocity_hammerhead.mk \
     $(LOCAL_DIR)/velocity_flo.mk \
-    $(LOCAL_DIR)/velocity_manta.mk \
-    $(LOCAL_DIR)/velocity_grouper.mk
  
 
--- products/velocity_hammerhead.mk
+++ products/velocity_hammerhead.mk
@@ -11,8 +11,5 @@ PRODUCT_DEVICE := hammerhead
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 4.4.2 KOT49H 937116 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT="google/hammerhead/hammerhead:4.4.3/KTU84M/1158763:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4.3 KTU84M 1158763 release-keys"
 
-# Proprietary libmmcamera_interface.so because of green bar bug
-PRODUCT_COPY_FILES += \
-    vendor/velocity/proprietary/system/lib/libmmcamera_interface.so:system/lib/libmmcamera_interface.so
