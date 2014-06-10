d611bfd2d02fa9c50136ad3ce86d4759c901c10b
ctfrommn <chadclough@gmail.com>
2014-06-10 12:30:41 -0500

    Seems the green line recording fix is still needed

--- products/velocity_hammerhead.mk
+++ products/velocity_hammerhead.mk
@@ -12,4 +12,6 @@ PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT="google/hammerhead/hammerhead:4.4.3/KTU84M/1158763:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4.3 KTU84M 1158763 release-keys"
-
+# Proprietary libmmcamera_interface.so because of green bar bug
+PRODUCT_COPY_FILES += \
+    vendor/velocity/proprietary/system/lib/libmmcamera_interface.so:system/lib/libmmcamera_interface.so
